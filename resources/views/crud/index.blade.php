21fd27f3b2f209071994627c9eaace1e6b23af9e
Luis <luis.romo@cceo.com.mx>
2025-05-15 13:50:10 -0600

    feat: :sparkles: : first routes added

---
+++ resources/views/crud/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mira</title>
+</head>
+<body>
+    <h1>crud</h1>
+ <h3>
+    <?php echo $name ;
+    echo $age?> 
+
+    {{$age}}
+    {{$name}}
+
+    
+ </h3>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return 'welcome';
+});
+
+Route::get('/test', function () {
+     return view('test');
+ });
+
+// Route::get('/crud', function () {
+//     return view('crud/index');
+// });
+
+
+Route::get('/crud', function () {
+    
+    $age = 20;
+    $data = ['name' => 'luis', 'age' => $age];
+
+    return view('crud/index', $data);
+})->name('crudy');
