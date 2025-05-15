ee3d1264a7c3903753461f00cb1f1298799d6d8c
Luis <luis.romo@cceo.com.mx>
2025-05-15 17:03:30 -0600

    feat: :sparkles: :switching from SQLite to MySQL

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return 'welcome';
+});
+
+Route::get('/test', function () {
+     return view('test');
+ });
+
+// Route::get('/crud', function () {
+//     return view('crud/index');
+// });
+
+
+Route::get('/crud', function () {
+    
+    $age = 20;
+    $data = ['name' => 'luis', 'age' => $age];
+
+    return view('crud/index', $data);
+})->name('crudy');
